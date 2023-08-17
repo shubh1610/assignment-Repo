54cec4b63ffdc7b8f96ee247afe6a058b3c1ed27
Shubham <77381294+shubh1610@users.noreply.github.com>
2023-08-17 18:24:43 +0530

    Update context.js

--- src/context.js
+++ src/context.js
@@ -1,71 +1,78 @@
-import { createContext,useState, useCallback, useEffect } from "react";
+import { createContext, useState, useCallback, useEffect } from "react";
 import axios from "axios";
-import Cookies from 'universal-cookie';
+import Cookies from "universal-cookie";
 const cookies = new Cookies();
 
 export const AuthContext = createContext();
-export const serverUrl ="https://zany-jade-panther-fez.cyclic.cloud:8080";
+export const serverUrl = "https://zany-jade-panther-fez.cyclic.cloud";
 
 export const AuthContextProvider = ({ children }) => {
   const [loggedIn, setLoggedIn] = useState(false);
   const [getProfileBlog, setGetProfileBlog] = useState(false);
   const [user, setUser] = useState();
   const [blogs, setBlogs] = useState([]);
- 
 
   const checkLoginState = () => {
     try {
       fetch(`${serverUrl}/auth/logged_in`, {
-        method: 'POST',
-        credentials: 'include',
-        headers:{
-          'Content-Type':'application/json'
-        }
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
       })
-      .then(response => response.json())
-      .then(body =>
-        {
+        .then((response) => response.json())
+        .then((body) => {
           setLoggedIn(body.loggedIn);
-          setUser(body.user)
-        }
-       )
-        axios.get(`${serverUrl}/getblogs`)
-        .then(res=>setBlogs(res.data))
-        .catch(err=>console.log(err))
+          setUser(body.user);
+        });
+      axios
+        .get(`${serverUrl}/getblogs`)
+        .then((res) => setBlogs(res.data))
+        .catch((err) => console.log(err));
     } catch (err) {
       console.error(err);
     }
   };
-  const logOut=()=>{
+  const logOut = () => {
     try {
       fetch(`${serverUrl}/auth/logout`, {
-        method: 'POST',
-        credentials: 'include',
-        headers:{
-          'Content-Type':'application/json'
-        }
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
       })
-      .then(response => response.json())
-      .then(body =>
-        {
+        .then((response) => response.json())
+        .then((body) => {
           setLoggedIn(body.loggedIn);
-          setUser({})
-        }
-       )
+          setUser({});
+        });
     } catch (err) {
       console.error(err);
     }
-  }
-  const getUserPost=()=>{
-    axios.get(serverUrl+'/getblogsbyid',{params:{user:user.email}})
-    .then(res=>setBlogs(res.data))
-    .catch(err=>console.log(err));
-  }
-
+  };
+  const getUserPost = () => {
+    axios
+      .get(serverUrl + "/getblogsbyid", { params: { user: user.email } })
+      .then((res) => setBlogs(res.data))
+      .catch((err) => console.log(err));
+  };
 
   return (
-    <AuthContext.Provider value={{ loggedIn, checkLoginState, blogs,logOut,user,getUserPost,setGetProfileBlog,getProfileBlog }}>
+    <AuthContext.Provider
+      value={{
+        loggedIn,
+        checkLoginState,
+        blogs,
+        logOut,
+        user,
+        getUserPost,
+        setGetProfileBlog,
+        getProfileBlog,
+      }}
+    >
       {children}
     </AuthContext.Provider>
   );
-}
+};
